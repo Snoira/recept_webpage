e49f3b48b39633732d3e8ed2d1b1ea099a39b5af
Nora Velde <noravelde@gmail.com>
2024-10-01 16:12:51 +0200

    -

--- backend/app/routes/recepieUnits.route.js
+++
@@ -1,8 +0,0 @@
-const Express = require('express');
-const {getRecepieUnits} = require('../controllers/recepieUnits.controller.js');
-
-const recepieUnitsRouter = Express.Router();
-
-recepieUnitsRouter.get("/", getRecepieUnits)
-
-module.exports = recepieUnitsRouter;
